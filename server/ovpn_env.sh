969fef50660b99d3aab98958f719b65975730ddf
Derek Pedersen <derekpedersen.com@gmail.com>
2018-11-10 21:16:22 -0800

    Added the deployment.yaml file.

---
+++ server/ovpn_env.sh
@@ -0,0 +1,25 @@
+declare -x OVPN_AUTH=SHA384
+declare -x OVPN_CIPHER=AES-256-GCM
+declare -x OVPN_CLIENT_TO_CLIENT=
+declare -x OVPN_CN=VPN.DEREKPEDERSEN.COM
+declare -x OVPN_COMP_LZO=0
+declare -x OVPN_DEFROUTE=1
+declare -x OVPN_DEVICE=tun
+declare -x OVPN_DEVICEN=0
+declare -x OVPN_DISABLE_PUSH_BLOCK_DNS=1
+declare -x OVPN_DNS=1
+declare -x OVPN_DNS_SERVERS=([0]="185.121.177.177" [1]="185.121.177.53" [2]="87.98.175.85")
+declare -x OVPN_ENV=/etc/openvpn/ovpn_env.sh
+declare -x OVPN_EXTRA_CLIENT_CONFIG=()
+declare -x OVPN_EXTRA_SERVER_CONFIG=()
+declare -x OVPN_FRAGMENT=
+declare -x OVPN_KEEPALIVE='10 60'
+declare -x OVPN_MTU=
+declare -x OVPN_NAT=0
+declare -x OVPN_PORT=31304
+declare -x OVPN_PROTO=udp
+declare -x OVPN_PUSH=()
+declare -x OVPN_ROUTES=([0]="192.168.254.0/24")
+declare -x OVPN_SERVER=192.168.255.0/24
+declare -x OVPN_SERVER_URL=udp://VPN.DEREKPEDERSEN.COM:31304
+declare -x OVPN_TLS_CIPHER=TLS-ECDHE-ECDSA-WITH-AES-256-GCM-SHA384
